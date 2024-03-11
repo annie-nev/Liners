9cc1adf94a55940b1948869c036905ed0b8a8653
Annie <annienabwni46@gmail.com>
2024-03-11 10:59:54 +0200

    mul_table function finished

--- liners_functions.py
+++ liners_functions.py
@@ -1,1 +1,2 @@
 str_len = lambda lst: list(map(len, lst))
+mul_table = lambda number: [i * number for i in range(1, number + 1)]
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from liners_functions import str_len
+from liners_functions import str_len, mul_table
 
 
 def main():
     print(str_len(["hello", "world", "kerem", "course"]))
+    print(mul_table(7))
 
 
 if __name__ == '__main__':
